9d837cfc73d903299c1b9d4ad8d3cb0c21cc89d5
Dmytrii9Cherniak <dcheyrnak10@gmail.com>
2024-08-10 01:16:19 +0300

    added table users component and modal component, installed toastr for notifications and other changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ModalComponent } from './components/modal/modal.component';
+
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+import { UserTableListComponent } from './components/user-table-list/user-table-list.component';
+import { ToastrModule } from 'ngx-toastr';
+import { toastrConfig } from './configs/toastr.config';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModalComponent,
+    UserTableListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ToastrModule.forRoot(toastrConfig),
   ],
-  providers: [],
+  exports: [
+    ModalComponent
+  ],
+  providers: [
+    provideHttpClient(),
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
